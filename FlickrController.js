48ccdb4f300b12cf523ac79c5ad140626eb016f3
Arun Nayak <arun@Aruns-MacBook-Air.local>
2016-08-19 23:29:27 +1000

    getting data from services

--- FlickrController.js
+++ FlickrController.js
@@ -1,24 +1,16 @@
 'use strict';
 
 flickrApp.controller('flickrController',
-  function FlickrController($scope, $http) {
+  function FlickrController($scope, $http, photosList) {
 
     $scope.tiles = {};
     $scope.showLoading = false;
 
-    $scope.searchTags = function() {
-      $scope.showLoading = true;
-      var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=JSON_CALLBACK";
-      flickrAPI = flickrAPI + "&tags" + encodeURIComponent($scope.searchTagsElm) + "&format=json";
-
-      $http.jsonp(flickrAPI)
-        .success(function(data, status, headers, config) {
-          $scope.tiles = data;
-          $scope.showLoading = false;
-        })
-        .error(function(data, status, headers, config) {
-          window.alert('Something went wrong, please try again later!');
-          $scope.showLoading = false;
+    $scope.searchTags = function(flickrAPI) {
+    $scope.showLoading = true;
+      photosList.getPhotos($scope.searchTagsElm).then(function(data){
+            $scope.tiles = data;
+            $scope.showLoading = false;
         });
 
       //Clear function
--- FlickrService.js
+++ FlickrService.js
@@ -1,22 +1,24 @@
 'use strict';
 
-flickrApp.factory('photos', function($http, $q) {
+flickrApp.factory('photosList', function($http, $q) {
        return {
-         getPhotos: function() {
-           var deferred = $q.defer();
-
-           var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=JSON_CALLBACK";
-               flickrAPI = flickrAPI + "&tags" + encodeURIComponent($scope.searchTagsElm) + "&format=json";
        
-           
-         $http.get(flickrAPI).success(function (data, status, headers, config) {
-                console.log(data);
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                //this always gets called
-                console.log(status);
-                deferred.reject(status);
-            });
-            return deferred.promise;
+       getPhotos: function(val) {
+       var deferred = $q.defer();
 
-     }  
+       var url = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=JSON_CALLBACK&tags=' + encodeURIComponent(val) + '&format=json';
+       $http.jsonp(url)
+       .success(function (data, status, headers, config) {
+              console.log(data);
+              deferred.resolve(data);
+          }).error(function (data, status, headers, config) {
+              //this always gets called
+              console.log(status);
+              deferred.reject(status);
+          });
+          return deferred.promise;
+        }
+
+
+    }  
+ });
